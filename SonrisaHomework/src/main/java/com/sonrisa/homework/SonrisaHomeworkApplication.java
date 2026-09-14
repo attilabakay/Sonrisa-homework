fe3526c3abcdb60470a643db4ed8ec27abcb56b2
Attila Bakay <bakay.attila.ferenc@gmail.com>
2026-09-14 21:54:33 +0200

    Add spring boot project for backend

---
+++ SonrisaHomework/src/main/java/com/sonrisa/homework/SonrisaHomeworkApplication.java
@@ -0,0 +1,13 @@
+package com.sonrisa.homework;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SonrisaHomeworkApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SonrisaHomeworkApplication.class, args);
+    }
+
+}
